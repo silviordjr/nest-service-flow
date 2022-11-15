179fa9273ce09b62bc6e6b2f69f1d4ba32facb27
silviordjr <silviordjr@outlook.com>
2022-11-15 08:41:38 -0300

    endpoint de criacao dos usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, UseFilters } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpExceptionFilter } from 'src/exceptions/http-exception.filter';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseFilters(HttpExceptionFilter)
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    try {
+      return this.usersService.create(createUserDto);
+    } catch (error) {
+      throw new HttpException(error.message, error.status)
+    };
   }
 
   @Patch(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import CustomError from 'src/exceptions/CustomError';
+import { HashManager } from 'src/helpers/HashManager';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectRepository(User)
+  private userRepo: Repository<User>,
+  ){}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const {name, login, password, role} = createUserDto;
+
+    if (!name || !login || !password || !role){
+      throw new CustomError(HttpStatus.UNPROCESSABLE_ENTITY, 'Campos incompletos.')
+    }
+
+    const hashedPass = new HashManager().createHash(createUserDto.password)
+    createUserDto.password = hashedPass
+
+    const user = this.userRepo.create(createUserDto)
+
+    return this.userRepo.save(user);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
