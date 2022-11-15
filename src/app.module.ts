74cf4429f9eb7e4c37f88524f26480057446ea64
silviordjr <silviordjr@outlook.com>
2022-11-14 22:50:01 -0300

    resources e pacotes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
